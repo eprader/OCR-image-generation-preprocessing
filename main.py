33799bc71d16004c3bec6d021733d4434f2dd216
eprader <56026248+eprader@users.noreply.github.com>
2023-08-10 02:19:16 +0300

    add text sequence generation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import image_create
 import preprocess
+import text_generator
 import numpy as np
 import cv2 as cv
+import string
 
 FONT_PATH = preprocess.FONT_PATH
 
@@ -10,4 +12,9 @@ if __name__ == "__main__":
     image = np.array(image_create.createimage(text, FONT_PATH, angle_degrees=180))
     cv.imwrite(f"{text}_render.png", image)
     preprocess.preprocess(text + "_render")
-
+    character_string = string.ascii_lowercase + " \n"
+    generator = text_generator.Generator([*character_string], 42)
+    print(generator.generate_random_sequence())
+    print(generator.generate_random_sequence())
+    print(generator.generate_random_sequence())
+    print(generator.generate_random_sequence())
---
+++ text_generator.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Generator:
+    def __init__(self, character_list, random_seed):
+        self.character_list = character_list
+        self.random_seed = random_seed
+        np.random.seed(random_seed)
+
+    def generate_random_sequence(self):
+
+        sequence = ""
+        previous_char = None
+
+        length = np.random.choice(range(1, 50))
+
+        for _ in range(length):
+            char = np.random.choice(self.character_list)
+            if char == ' ' or char == '\n':
+                if previous_char != ' ' or None or '\n':
+                    sequence += '\n'
+            else:
+                sequence += char
+
+            previous_char = char
+
+        return sequence
