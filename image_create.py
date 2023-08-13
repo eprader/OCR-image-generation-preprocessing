12ae4e94aef609f4372ff18b099b3250e2203d1f
eprader <56026248+eprader@users.noreply.github.com>
2023-08-14 01:38:51 +0300

    move image create to oop

--- image_create.py
+++ image_create.py
@@ -1,35 +1,58 @@
 from PIL import Image, ImageDraw, ImageFont
 
-BG_COLOR = (0, 0, 0) # black
-FG_COLOR = (255, 255, 255) # white
+class ImageGenerator:
+    def __init__(self, font_path, font_size, padding=10, bg_color=(0, 0, 0), fg_color=(255, 255, 255)):
+        self.font = ImageFont.truetype(font_path, font_size)
+        self.bg_color = bg_color
+        self.fg_color = fg_color
+        self.padding = padding
 
-def get_font_text_size(font, text):
-    image = Image.new("RGB", (1, 1))
-    draw = ImageDraw.Draw(image)
+    def _get_font_text_size(self, text):
+        """
+        Returns the projected width and height of the text to be drawn.
 
-    width, height = draw.textsize(text, font=font)
+        Parameters:
+            text (str): The text to be measured in size.
 
-    return width, height
+        Returns:
+            width (int): width of the text if rendered (in pixels).
+            height (int): height of the text if rendered (in pixels).
+        """
 
-def draw_text_image(text, font, text_padding, background_color=BG_COLOR, font_color=FG_COLOR):
-    width, height = get_font_text_size(font, text)
-    image = Image.new("RGB", (width + 2 * text_padding, height + 2 * text_padding), background_color)
-    draw = ImageDraw.Draw(image) 
-    text_x_y = (text_padding, text_padding)
+        image = Image.new("RGB", (1, 1))
+        draw = ImageDraw.Draw(image)
 
-    draw.text(text_x_y, text, font=font, fill=font_color)
+        width, height = draw.textsize(text, font=self.font)
 
-    return image
+        return width, height
 
-def rotate_image(image, angle_degrees, fillcolor=BG_COLOR):
-    rotated_text = image.rotate(angle_degrees, resample=Image.BICUBIC, expand=True, fillcolor=fillcolor)
+    def _draw_text_image(self, text,):
+        """
+        Creates an image containing the given text.
+        
+        Parameters:
+            text (str): The text to be contained in the image.
 
-    return rotated_text
+        Returns:
+            
+        """
+        width, height = self._get_font_text_size(text)
+        image = Image.new("RGB", (width + 2 * self.padding, height + 2 * self.padding), self.bg_color)
+        draw = ImageDraw.Draw(image) 
+        text_x_y = (self.padding, self.padding)
 
-def createimage(text, font_path, font_size=40, text_padding=10, angle_degrees = 0):
-    font = ImageFont.truetype(font_path, font_size)
-    image = draw_text_image(text, font, text_padding=text_padding)
+        draw.text(text_x_y, text, font=self.font, fill=self.fg_color)
 
-    rotated = rotate_image(image, angle_degrees)
+        return image
 
-    return rotated
+    def _rotate_image(self, image, angle_degrees):
+        rotated_text = image.rotate(angle_degrees, resample=Image.BICUBIC, expand=True, fillcolor=self.bg_color)
+
+        return rotated_text
+
+    def createimage(self, text, rotation_angle_degrees = 0):
+        image = self._draw_text_image(text)
+
+        rotated = self._rotate_image(image, rotation_angle_degrees)
+
+        return rotated
